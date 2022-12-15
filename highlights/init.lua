0441ac404762146a2a69e16bb35539a1caa364de
Nawfal <nawfalmrouyan@gmail.com>
2022-12-16 04:16:05 +0800

    NvChad telescope theme

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,4 +1,9 @@
--- Add highlight groups in any theme
+-- get highlights from highlight groups
+local normal = astronvim.get_hlgroup "Normal"
+local fg, bg = normal.fg, normal.bg
+local bg_alt = astronvim.get_hlgroup("Visual").bg
+local green = astronvim.get_hlgroup("String").fg
+local red = astronvim.get_hlgroup("Error").fg -- Add highlight groups in any theme
 return {
   BufferLineTabClose = { fg = "#1e222a", bg = "#1e222a" },
   BufferLineBufferSelected = { fg = "#abb2bf", bg = "#1e222a" },
@@ -65,4 +70,16 @@ return {
   SpellCap = { sp = "#ffbba6", undercurl = true },
   SpellLocal = { sp = "#ffbba6", undercurl = true },
   SpellRare = { sp = "#ffbba6", undercurl = true },
+  TelescopeBorder = { fg = bg_alt, bg = bg },
+  TelescopeNormal = { bg = bg },
+  TelescopePreviewBorder = { fg = bg, bg = bg },
+  TelescopePreviewNormal = { bg = bg },
+  TelescopePreviewTitle = { fg = bg, bg = green },
+  TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+  TelescopePromptNormal = { fg = fg, bg = bg_alt },
+  TelescopePromptPrefix = { fg = red, bg = bg_alt },
+  TelescopePromptTitle = { fg = bg, bg = red },
+  TelescopeResultsBorder = { fg = bg, bg = bg },
+  TelescopeResultsNormal = { bg = bg },
+  TelescopeResultsTitle = { fg = bg, bg = bg },
 }
