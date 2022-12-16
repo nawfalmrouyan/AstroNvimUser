69038908e959f99385c9b5c3f3e3f415d076f541
Nawfal <nawfalmrouyan@gmail.com>
2022-12-17 01:58:19 +0800

    add lf plugin

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,4 +1,21 @@
 return {
+  {
+    "lmburns/lf.nvim",
+    config = function()
+      -- This feature will not work if the plugin is lazy-loaded
+      vim.g.lf_netrw = 1
+
+      require("lf").setup {
+        escape_quit = false,
+        border = "rounded",
+        highlights = { FloatBorder = { guifg = "#819c3b" } },
+        winblend = 0,
+      }
+
+      vim.keymap.set("n", "<C-o>", ":Lf<CR>")
+    end,
+    requires = { "plenary.nvim", "toggleterm.nvim" },
+  },
   { "max397574/better-escape.nvim", disable = true },
   { "rcarriga/nvim-notify", diable = true },
   { "folke/zen-mode.nvim", cmd = { "ZenMode" }, config = function() require("user.plugins.zen-mode").config() end },
